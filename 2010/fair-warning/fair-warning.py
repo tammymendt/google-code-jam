0b912cfef81fa516b4a0e9b8c36c527269ee2b99
tammymendt <tammymendt@gmail.com>
2017-12-12 15:34:59 +0100

    adding code from code jam 2010

---
+++ 2010/fair-warning/fair-warning.py
@@ -0,0 +1,36 @@
+import sys
+import os
+from fractions import math
+from functools import reduce
+
+
+def solve_apocalypse(input_array):
+
+    t_array = []
+    for i in range(0, len(input_array)):
+        for j in range(i + 1, len(input_array)):
+            t = abs(input_array[i] - input_array[j])
+            if t != 0:
+                t_array.append(t)
+
+    T = reduce(lambda x, y: math.gcd(x, y), t_array)
+
+    if input_array[0] % T == 0:
+        return 0
+
+    return T - (input_array[0] % T)
+
+
+if __name__ == "__main__":
+
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    out_file = open(dir_path + '/out-large.txt','w')
+    in_file = open(sys.argv[1],'r')
+    in_file.readline()
+    case = 1
+    for line in in_file:
+        input = line.split(' ')
+        n = int(line[0])
+        result = solve_apocalypse(list(map(int, input[1:])))
+        out_file.write('Case #%s: %s\n' % (str(case), str(result)))
+        case += 1
